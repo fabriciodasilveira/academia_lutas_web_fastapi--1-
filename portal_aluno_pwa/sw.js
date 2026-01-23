a74fcd9d3077ce07ef0a54a691908d3c611e604b
Fabricio Silveira <fabvass@br.ibm.com>
2026-01-23 11:19:56 -0300

    Update sw.js

--- portal_aluno_pwa/sw.js
+++ portal_aluno_pwa/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'aluno-portal-v1.15'; // Mudei a versão para forçar atualização
-const IMAGES_CACHE_NAME = 'aluno-images-v1'; // Cache separado para imagens
+const CACHE_NAME = 'aluno-portal-v1.16'; // Mudei a versão para forçar atualização
+const IMAGES_CACHE_NAME = 'aluno-images-v2'; // Cache separado para imagens
 
 const urlsToCache = [
     '/portal',
